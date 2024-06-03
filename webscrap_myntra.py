6f4fea464dbfec2a86474dd1da66ce52993a8ad6
Shaik Mohammad, Rafi <Rafi.ShaikMohammad@harman.com>
2024-06-03 15:34:38 +0530

    new feature on brands filter

--- webscrap_myntra.py
+++ webscrap_myntra.py
@@ -6,7 +6,7 @@ product_list=[]
 product_links=[]
 product_price=[]
 elements = driver.find_elements_by_class_name("product-base")
-for element in elements:
+for element in elements:   #Filtering all brands
     product_list.append((element.find_element_by_class_name("product-brand").text))
     product_price.append((element.find_element_by_class_name("product-price").text))
     product_links.append(element.find_element_by_tag_name("a").get_attribute("href"))
